e1c55a4b0ad9ca1661886a0a1c4d18313d272e84
vamsi chunduru <vamsi chunduru>
2021-07-15 00:27:28 +0200

    robo friend initial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+
+class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+
+    onSeachChange = (event) => {
+        this.setState({ searchField: event.target.value })
+
+    }
+
+    render() {
+
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        });
+
+        return (
+            <div className='tc' >
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSeachChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'tachyons';
+
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import './index.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    < div>
+      <App />
+    </div>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
